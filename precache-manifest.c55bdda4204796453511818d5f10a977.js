fb197a0c3968aa437672b070861c6847fc841a8c
dipakroy7577 <deepak.roy7577@gmail.com>
2020-12-02 21:48:34 -0800

    Updates

---
+++ precache-manifest.c55bdda4204796453511818d5f10a977.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99eacbc73fb29603ed31ea8a636f792",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "917aa60190d97abab592",
+    "url": "/reactlivewebsite/static/css/main.ae3c283b.chunk.css"
+  },
+  {
+    "revision": "f08b574b4af1f6eb120b",
+    "url": "/reactlivewebsite/static/js/2.11d713f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactlivewebsite/static/js/2.11d713f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917aa60190d97abab592",
+    "url": "/reactlivewebsite/static/js/main.1266824b.chunk.js"
+  },
+  {
+    "revision": "69237cd1b6155c1c7b41",
+    "url": "/reactlivewebsite/static/js/runtime-main.99d0577d.js"
+  },
+  {
+    "revision": "1044a68b576ac1e2bb5b4609e1b594b7",
+    "url": "/reactlivewebsite/static/media/svg-1.1044a68b.svg"
+  },
+  {
+    "revision": "5c246e8e5860b58388d956f049450c4c",
+    "url": "/reactlivewebsite/static/media/svg-5.5c246e8e.svg"
+  },
+  {
+    "revision": "4491c8c98aa7f5545754c74367b65225",
+    "url": "/reactlivewebsite/static/media/svg-7.4491c8c9.svg"
+  },
+  {
+    "revision": "c78a1bf247367c615eff109430742e97",
+    "url": "/reactlivewebsite/static/media/svg-8.c78a1bf2.svg"
+  }
+]);
